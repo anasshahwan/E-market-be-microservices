b4e384f8990dba581da91eaac8dadbbb9173f771
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2022-05-05 22:13:36 -0400

    setting up the company routes

--- company/routes/company.js
+++ company/routes/company.js
@@ -13,4 +13,24 @@ router.post("/", (req, res, next) => {
   });
 });
 
+router.get("/:companyId", (req, res, next) => {
+  const id = req.params.companyId;
+  res.status(200).json({
+    message: "Get a Company By " + id,
+  });
+});
+
+router.patch("/:companyId", (req, res, next) => {
+  const id = req.params.companyId;
+  res.status(200).json({
+    message: "Update a Company By " + id,
+  });
+});
+
+router.delete("/:companyId", (req, res, next) => {
+  const id = req.params.companyId;
+  res.status(200).json({
+    message: "Delete a Company By " + id,
+  });
+});
 module.exports = router;
