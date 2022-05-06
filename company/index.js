5ea2afdf12d1791e1af76d61992c9a7615ecc4be
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2022-05-05 22:54:12 -0400

    mongodb was connected & env file was added

--- company/index.js
+++ company/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const app = express();
 const port = 8080;
-
 const companyRoutes = require("./routes/company");
+
+mongoose.connect(
+  "mongodb+srv://anas:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0.gxqhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
 
 app.get("/", (req, res) => {
   res.send("Hello Company Api");
