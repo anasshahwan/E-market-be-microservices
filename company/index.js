ddc2312e2ce3c4a8b4a5d62332e5fc565c001691
anas shahwan <anas.shahwan20@gmail.com>
2022-05-15 19:08:13 -0400

    send message to rabbitmq QUEUE

--- company/index.js
+++ company/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const amqp = require("amqplib");
 const app = express();
 const port = 8080;
 
@@ -17,7 +18,20 @@ mongoose.connect(
 );
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+async function connect() {
+  const msg = { id: 4 };
+  try {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    const result = await channel.assertQueue("e-market");
+    channel.sendToQueue("e-market", Buffer.from(JSON.stringify(msg)));
+    console.log("Job send successfully");
+  } catch (ex) {
+    console.log(ex);
+  }
+}
 
+connect();
 const swaggerOptions = {
   swaggerDefinition: {
     components: {},
